3dfc29526198ab22b8234a93f5d202dd72299548
Marcin Bogiel <marcinb118@gmail.com>
2018-06-09 23:06:33 +0200

    Disable classicalBitArrow on start

---
+++ dotBloch/Assets/codeBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class codeBase : MonoBehaviour {
+
+	public GameObject classicalBitArrow; 
+	// Use this for initialization
+	void Start () {
+		classicalBitArrow.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
