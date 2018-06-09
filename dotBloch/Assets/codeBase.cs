8debed61a40e372f082e5edaf6888f7a370d73f3
Marcin Bogiel <marcinb118@gmail.com>
2018-06-09 23:14:09 +0200

    disableClassicalBit() method added

--- dotBloch/Assets/codeBase.cs
+++ dotBloch/Assets/codeBase.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class codeBase : MonoBehaviour {
 
+	public GameObject classicalOne;
+	public GameObject classicalZero;
 	public GameObject classicalBitArrow; 
+
 	// Use this for initialization
 	void Start () {
-		classicalBitArrow.SetActive(false);
+		disableClassicalBit();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void disableClassicalBit(){
+		classicalOne.SetActive (false);
+		classicalZero.SetActive (false);
+		classicalBitArrow.SetActive(false);
 	}
 }
