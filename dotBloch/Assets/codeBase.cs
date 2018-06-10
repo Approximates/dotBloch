a0a3c8753709dacc44e62f39ad0bc4e7904a3ade
Marcin Bogiel <marcinb118@gmail.com>
2018-06-10 11:48:26 +0200

    Switching buttons color

--- dotBloch/Assets/codeBase.cs
+++ dotBloch/Assets/codeBase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class codeBase : MonoBehaviour {
 
@@ -15,7 +16,8 @@ public class codeBase : MonoBehaviour {
 	public GameObject quantumBitZero;
 	public GameObject quantumBitArrow;
 
-
+	public Button switchToClassicalBit;
+	public Button switchToQuantumBit;
 
 
 	// Use this for initialization
@@ -35,12 +37,27 @@ public class codeBase : MonoBehaviour {
 		classicalOne.SetActive (true);
 		classicalZero.SetActive (true);
 		classicalBitArrow.SetActive(true);
+
+		switchToClassicalBit.GetComponent<Image>().color = Color.yellow; 
+	}
+
+	public void enableQuantumBit(){
+		circleX.SetActive(true);
+		circleY.SetActive(true);
+		circleZ.SetActive(true);
+		quantumBitOne.SetActive(true);
+		quantumBitZero.SetActive(true);
+		quantumBitArrow.SetActive(true);
+
+		switchToQuantumBit.GetComponent<Image>().color = Color.yellow; 
 	}
 
 	public void disableClassicalBit(){
 		classicalOne.SetActive (false);
 		classicalZero.SetActive (false);
 		classicalBitArrow.SetActive(false);
+
+		switchToClassicalBit.GetComponent<Image>().color = Color.white; 
 	}
 
 	public void disableQuantumBit(){
@@ -50,5 +67,7 @@ public class codeBase : MonoBehaviour {
 		quantumBitOne.SetActive(false);
 		quantumBitZero.SetActive(false);
 		quantumBitArrow.SetActive(false);
+
+		switchToQuantumBit.GetComponent<Image>().color = Color.white;
 	}
 }
