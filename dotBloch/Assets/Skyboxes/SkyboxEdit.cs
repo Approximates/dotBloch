ea453b9517b4e211e69799b77926fa44596ae0c4
AdamGawienczuk <32729112+AdamGawienczuk@users.noreply.github.com>
2018-08-19 16:04:23 +0200

    change skybox method added

---
+++ dotBloch/Assets/Skyboxes/SkyboxEdit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxEdit : MonoBehaviour {
+
+    public Material black;
+    public Material starsDefault;
+    public Material starsLightBlue;
+    public Material starsRed;
+
+    // Use this for initialization
+    void Start () {
+
+        ChangeSkybox(starsLightBlue);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeSkybox(Material skybox)
+    {
+        RenderSettings.skybox = skybox;
+    }
+
+}
