897490d621d7f00ef516ef587e813b8fb0b39bbf
marcinbogiel <marcin.bogiel@gmail.com>
2019-11-12 20:11:59 +0100

    Theta change tests have been added

--- dotBloch/Assets/Classes/Tests/printOneValueTests.cs
+++ dotBloch/Assets/Classes/Tests/printOneValueTests.cs
@@ -101,5 +101,36 @@ namespace Tests
             settings = new PrintBlochSettings(false,false,3,PrintBlochSettings.DecimalSeparator.comma,PrintBlochSettings.ImaginaryUnit.j);
             Assert.AreEqual("0,271+0,271j", quantumBit.printOneValue(false,settings));
         }
+
+        [Test]
+        public void theta_change_0_to_30(){
+            quantumBit = new Qubit(0,120);
+            Assert.AreEqual("0",quantumBit.printOneValue(false));
+            quantumBit.thetaAngle = 30;
+            Assert.AreEqual("- 130 + 0,224i",quantumBit.printOneValue(false));
+        }
+
+        [Test]
+        public void theta_change_30_to_90(){
+            quantumBit = new Qubit(30,90);
+            Assert.AreEqual("0,259i",quantumBit.printOneValue(false));
+            quantumBit.thetaAngle = 90;
+            Assert.AreEqual("+ 0,707i",quantumBit.printOneValue(true));
+        }
+
+        [Test]
+        public void theta_change_90_to_120(){
+            quantumBit = new Qubit(90,30);
+            Assert.AreEqual("0,612 + 0,354i",quantumBit.printOneValue(false));
+            quantumBit.thetaAngle = 120;
+            Assert.AreEqual("0,750 + 0,433i",quantumBit.printOneValue(false));
+        }
+        [Test]
+        public void theta_change_120_to_170(){
+            quantumBit = new Qubit(120,140);
+            Assert.AreEqual("- 0,663 + 0,557",quantumBit.printOneValue(false));
+            quantumBit.thetaAngle = 170;
+            Assert.AreEqual("- 0,763 + 0,640",quantumBit.printOneValue(false));
+        }
     }
 }
