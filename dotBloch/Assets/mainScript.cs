26a3128d1f122fa707b806aab63707a2b2a3f6c6
marcinbogiel <marcin.bogiel@gmail.com>
2019-12-15 22:52:57 +0100

    [add] Default app resolution has been defined

--- dotBloch/Assets/mainScript.cs
+++ dotBloch/Assets/mainScript.cs
@@ -38,6 +38,7 @@ public class mainScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Screen.SetResolution(1280,720,false);
 		quantumBit = new Qubit(45,0);
         thetaInputField.characterLimit = 6;
 		phiInputField.characterLimit = 6;
