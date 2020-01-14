5b45ebe70095d1e659aa5027ef41e8f5e3842899
AdamGawienczuk <32729112+AdamGawienczuk@users.noreply.github.com>
2020-01-14 14:54:38 +0100

    Exit cofirmation pop up added

--- dotBloch/Assets/Classes/StaticMethods.cs
+++ dotBloch/Assets/Classes/StaticMethods.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 
-class StaticMethods
+public class StaticMethods
 {
     
     public static double degree_to_radian(double angle, int? decimalSpaces = null){
         return Math.PI * angle / 180.0;
     }
-    
+
+    public static string exitText = "Do you want to close the app?";
+
 }
 
---
+++ dotBloch/Assets/exitConfirmation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitConfirmation : MonoBehaviour
+{
+    public GameObject ExitPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ExitButtonYes_Click()
+    {
+        Application.Quit();
+    }
+
+    public void ExitButtonNo_Click()
+    {
+        ExitPanel.SetActive(false);
+    }
+
+}
