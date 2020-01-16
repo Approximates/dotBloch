fc023e84247981dca06b76cebb9845bec809a158
marcinbogiel <marcin.bogiel@gmail.com>
2020-01-16 11:45:22 +0100

    [review] Unnecessary code removed

--- dotBloch/Assets/Interface.cs
+++ dotBloch/Assets/Interface.cs
@@ -6,18 +6,6 @@ public class Interface : MonoBehaviour
 {
     public GameObject ExitPanel;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void ExitButtonYes_Click()
     {
         Application.Quit();
@@ -27,5 +15,4 @@ public class Interface : MonoBehaviour
     {
         ExitPanel.SetActive(false);
     }
-
 }
