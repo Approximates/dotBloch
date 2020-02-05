1b29c888c9c1598e886b36c5ab5d9225595db643
AdamGawienczuk <32729112+AdamGawienczuk@users.noreply.github.com>
2020-02-05 19:48:15 +0100

    Added and implemented CopyToClipboard, LongClickButton and Vibrate classes

---
+++ dotBloch/Assets/Classes/CopyToClipboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RDG;
+
+public class CopyToClipboard : MonoBehaviour
+{
+    public GameObject text;
+    public void CopyTextToClipboard()
+    {
+        string txt = text.GetComponentInChildren<Text>().text;
+
+        TextEditor editor = new TextEditor
+        {
+            text = txt
+        };
+        editor.SelectAll();
+        editor.Copy();
+        RDG.Vibration.Vibrate(50);
+    }
+}
---
+++ dotBloch/Assets/Classes/LongClickButton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LongClickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	private bool pointerDown;
+	private float pointerDownTimer;
+
+	[SerializeField]
+	private float requiredHoldTime;
+
+	public UnityEvent onLongClick;
+
+	[SerializeField]
+	private Image fillImage;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		pointerDown = true;
+		Debug.Log("OnPointerDown");
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		Reset();
+		Debug.Log("OnPointerUp");
+	}
+
+	private void Update()
+	{
+		if (pointerDown)
+		{
+			pointerDownTimer += Time.deltaTime;
+			if (pointerDownTimer >= requiredHoldTime)
+			{
+				if (onLongClick != null)
+					onLongClick.Invoke();
+
+				Reset();
+			}
+			//fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+		}
+	}
+
+	private void Reset()
+	{
+		pointerDown = false;
+		pointerDownTimer = 0;
+		//fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+	}
+
+}
