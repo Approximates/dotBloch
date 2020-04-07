9a9aa6fe4b7b53acb6aaa08850d94935abb50ee3
marcinbogiel <marcin.bogiel@gmail.com>
2020-04-07 15:07:00 +0200

    Script renamed to SetAngleWithButton

--- dotBloch/Assets/Scripts/Buttons/SetAngleWithButton.cs
+++ dotBloch/Assets/Scripts/Buttons/SetAngleWithButton.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class SetPhiWithButton : MonoBehaviour
+public class SetAngleWithButton : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject recesiver;
     public double angle_to_send;
 
     public Text label;
+
+    public string method_to_update;
     public void send_angle(){
-        recesiver.SendMessage("set_phi_angle",angle_to_send);
+        recesiver.SendMessage(method_to_update,angle_to_send);
     }
 
     public void highlightLabel()
