96a660109fd33bb7c1879cc69aebc614e9c533d9
marcinbogiel <marcin.bogiel@gmail.com>
2020-04-07 13:50:04 +0200

    Highlighting phi button when pressed

--- dotBloch/Assets/Scripts/Buttons/SetPhiWithButton.cs
+++ dotBloch/Assets/Scripts/Buttons/SetPhiWithButton.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetPhiWithButton : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject recesiver;
     public double angle_to_send;
+
+    public Text label;
     public void send_angle(){
         recesiver.SendMessage("set_phi_angle",angle_to_send);
+    }
+
+    public void highlightLabel()
+    {
+        Debug.Log("Podświetlenie");
+        this.label.color = Color.red;
+    }
+
+    public void unhighligthLabel()
+    {
+        Debug.Log("Gaszenie");
+        this.label.color = Color.white;
     }
 }
