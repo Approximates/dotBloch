6348234cca94a0fd7988953bdad55f32165c0704
Jacek Cieszyński <jacox520@gmail.com>
2020-05-11 21:12:42 +0200

    fps counter class added
    Co-Authored-By: Marcin Bogiel <marcin.bogiel@gmail.com>

---
+++ dotBloch/Assets/Classes/FPSCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEditor;
+public class FPSCounter 
+{
+
+    public FPSCounter()
+    {
+        Debug.Log("is working");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ dotBloch/Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEditor;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public Text milisecondsLabel;
+    public Text fpsLabel;
+    public FPSCounter fps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fps = new FPSCounter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
