c3e8d52dd435ad4bf195eaea48bc757eecb66d22
Jacek Cieszyński <jacox520@gmail.com>
2020-05-14 21:36:36 +0200

    clean code refactor
    Co-Authored-By: Marcin Bogiel <marcin.bogiel@gmail.com>

--- dotBloch/Assets/Classes/FPSCounter.cs
+++ dotBloch/Assets/Classes/FPSCounter.cs
@@ -19,23 +19,15 @@ public class FPSCounter
         float fps = 1; 
         double fpsRatio = Math.Round((fps/deltaTime),0);
         if (fpsRatio <=23)
-        {
-            framesPerSecond.displayColor = new Color32(204,51,0,255);
-        }else if (fpsRatio >=24 && fpsRatio <=30)
-        {
-            framesPerSecond.displayColor = new Color32(255,102,0,255);
-
-        }else if (fpsRatio >=31 && fpsRatio <=48)
-        {
-            framesPerSecond.displayColor = new Color32(255,255,0,255);
-        }else if (fpsRatio >=49 && fpsRatio <=60)
-        {
-            framesPerSecond.displayColor = new Color32(0,153,0,255);
-        }else
-        {
-            framesPerSecond.displayColor = new Color32(0,204,0,255);
-        }
-
+           setLabelAsRed();
+        else if (fpsRatio >=24 && fpsRatio <=30)
+           setLabelAsOrange();
+        else if (fpsRatio >=31 && fpsRatio <=48)
+           setLabelAsYellow();
+        else if (fpsRatio >=49 && fpsRatio <=60)
+           setLabelAsGreen();
+        else setLabelAsLightGreen();
+        
         framesPerSecond.displayValue = fpsRatio.ToString() + " FPS";
         oneFrameExecuteTime.displayValue = Math.Round((deltaTime*1000),0).ToString() + " ms";
         oneFrameExecuteTime.displayColor = new Color32(255,255,255,255); 
@@ -50,5 +42,29 @@ public class FPSCounter
             this.oneSecond = 1;
             return true;
         }else return false;
+   }
+
+   private void setLabelAsRed()
+   {
+        this.framesPerSecond.displayColor = new Color32(204,51,0,255);
+   } 
+
+    private void setLabelAsOrange()
+   {
+        this.framesPerSecond.displayColor = new Color32(255,102,0,255);
+   }
+
+    private void setLabelAsYellow()
+   {
+        this.framesPerSecond.displayColor = new Color32(255,255,0,255);
+   }
+
+    private void setLabelAsGreen()
+   {
+        this.framesPerSecond.displayColor = new Color32(0,153,0,255);
+   }
+    private void setLabelAsLightGreen()
+   {
+        this.framesPerSecond.displayColor = new Color32(0,204,0,255);
    }
 }
