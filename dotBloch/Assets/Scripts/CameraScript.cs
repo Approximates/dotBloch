9e8fe590eb53f99afd628a0f21bf8d9aba3b920c
marcinbogiel <marcin.bogiel@gmail.com>
2020-05-18 19:02:59 +0200

    Code structured

--- dotBloch/Assets/Scripts/CameraScript.cs
+++ dotBloch/Assets/Scripts/CameraScript.cs
@@ -7,30 +7,23 @@ public class CameraScript : MonoBehaviour
 {
     void Start()
     {
-      double cameraAspectRatio =  gameObject.GetComponent<Camera>().aspect;
-      cameraAspectRatio = Math.Round(cameraAspectRatio,2);
+        double cameraAspectRatio =  gameObject.GetComponent<Camera>().aspect;
+        cameraAspectRatio = Math.Round(cameraAspectRatio,2);
 
-  if (cameraAspectRatio == 1.6) //16:10
-    {
-        gameObject.GetComponent<Camera>().transform.Rotate(0,-2,0);
-    }else if(cameraAspectRatio == 1.33) //4:3
-    {
-        gameObject.GetComponent<Camera>().transform.Rotate(0,2,0);
-        gameObject.GetComponent<Camera>().orthographicSize = 2.32f;
-    }else if (cameraAspectRatio == 1.25) //5:4
-    {
-         gameObject.GetComponent<Camera>().orthographicSize = 2.75f;
-         gameObject.GetComponent<Camera>().transform.Rotate(0,4,0);
-    }else if (cameraAspectRatio == 1.5) //3:2
-    {
-        gameObject.GetComponent<Camera>().transform.Rotate(0,2,0);
-        gameObject.GetComponent<Camera>().orthographicSize = 2.12f;
-    }
-}
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (cameraAspectRatio == 1.6) //16:10
+            gameObject.GetComponent<Camera>().transform.Rotate(0,-2,0);
+        else if(cameraAspectRatio == 1.33) //4:3
+        {
+            gameObject.GetComponent<Camera>().transform.Rotate(0,2,0);
+            gameObject.GetComponent<Camera>().orthographicSize = 2.32f;
+        }else if (cameraAspectRatio == 1.25) //5:4
+        {
+            gameObject.GetComponent<Camera>().orthographicSize = 2.75f;
+            gameObject.GetComponent<Camera>().transform.Rotate(0,4,0);
+        }else if (cameraAspectRatio == 1.5) //3:2
+        {
+            gameObject.GetComponent<Camera>().transform.Rotate(0,2,0);
+            gameObject.GetComponent<Camera>().orthographicSize = 2.12f;
+        }
     }
 }
