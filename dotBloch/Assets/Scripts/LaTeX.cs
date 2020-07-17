6fa01857b3308cda62391ea14e468544337ab25f
marcinbogiel <marcin.bogiel@gmail.com>
2020-07-17 18:20:31 +0200

    Copying probability values works

--- dotBloch/Assets/Scripts/LaTeX.cs
+++ dotBloch/Assets/Scripts/LaTeX.cs
@@ -6,10 +6,24 @@ using UnityEngine.UI;
 
 public class LaTeX : MonoBehaviour
 {
+    private string result;
     public void copyTheBlochVector()
     {
-        
-        string result = GameObject.Find("blochVector").GetComponent<Text>().text;
+        result = GameObject.Find("blochVector").GetComponent<Text>().text;
+        StaticMethods.copyToClipboard(result);
+        Debug.Log("copied: " + result);
+    }
+
+    public void copyZeroProbability()
+    {
+        result = GameObject.Find("text_zero_probability").GetComponent<Text>().text;
+        StaticMethods.copyToClipboard(result);
+        Debug.Log("copied: " + result);
+    }
+
+    public void copyOneProbability()
+    {
+        result = GameObject.Find("text_one_probability").GetComponent<Text>().text;
         StaticMethods.copyToClipboard(result);
         Debug.Log("copied: " + result);
     }
