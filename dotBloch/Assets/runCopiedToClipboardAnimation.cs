abfb4c7e93e727272af2742e24db693daed4f641
marcinbogiel <marcin.bogiel@gmail.com>
2020-07-31 12:37:32 +0200

    Animation works!

---
+++ dotBloch/Assets/runCopiedToClipboardAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runCopiedToClipboardAnimation : MonoBehaviour
+{
+    public void displayCopiedLabel()
+    {
+        Debug.Log("Uruchomilem animacje!");
+        Animator animation = gameObject.GetComponent<Animator>();
+        Debug.Log("Stan animatora: " + animation);
+
+        bool isAnimationRunning = animation.GetBool("isOpened");
+        animation.SetBool("isOpened", !isAnimationRunning);
+        Debug.Log("Animacja zakonczona!");
+    }
+}
