2d1234cc76b2ecdec5a1cbf8473fec29f52b0e0a
marcinbogiel <marcin.bogiel@gmail.com>
2020-08-09 14:27:58 +0200

    First tests for classical bit are implemented

---
+++ dotBloch/Assets/Classes/Tests/ClassicalBitTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+namespace Tests
+{
+    public class ClassicalBitTests
+    {
+        Bit classicalBit;
+        // A Test behaves as an ordinary method
+        [Test]
+        public void nUnit_Test()
+        {
+            Assert.AreEqual(true, true);
+            Assert.AreEqual(null, null);
+        }
+        [Test]
+        public void constructorTrue_Test()
+        {
+             classicalBit=new Bit(true);
+             Assert.AreEqual("1",classicalBit.getState());
+        }
+
+        [Test]
+        public void constructorFalse_Test()
+        {
+             classicalBit=new Bit(false);
+             Assert.AreEqual("0",classicalBit.getState());
+        }
+            
+    }
+}
