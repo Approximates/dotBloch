a10ba42b7216710559ad39a85e67b9b9dbe65eb4
marcinbogiel <marcin.bogiel@gmail.com>
2020-08-09 11:56:13 +0200

    Bit class is added

---
+++ dotBloch/Assets/Classes/Bit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bit
+{
+    private bool _value;
+
+    public Bit(bool state){
+        this._value = state;
+    }
+
+    public void NotGate(){
+        this._value = !this._value;
+    }
+
+    public void setOne(){
+        this._value = true;
+    }
+
+    public void setZero(){
+        this._value = false;
+    }
+}
