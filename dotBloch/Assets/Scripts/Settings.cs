afec27604acb84d355224ba757f4e4cb6d0a0843
AdamGawienczuk <32729112+AdamGawienczuk@users.noreply.github.com>
2020-08-21 15:34:24 +0200

    Settings pannel added

---
+++ dotBloch/Assets/Scripts/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    public void ToggleSettingsPanel()
+    {
+        settingsPanel.SetActive(!settingsPanel.active);
+    }
+}
